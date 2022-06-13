50a8a8b413db4a8ef98f7c3fe42dec4f55c7a563
Binh Pham <binh.pham2@mservice.com.vn>
2022-06-13 16:45:27 +0700

    home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hop_hui_online/colors.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    home: Scaffold(
+      body: MyApp(),
+    ),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -9,15 +14,30 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Hello world',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: Text('Hello world'),
-        ),
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      width: size.width,
+      height: size.height,
+      alignment: Alignment.center,
+      color: PRIMARY_BACKGROUND,
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Positioned(
+            bottom: 0,
+            child: Container(
+              alignment: Alignment.center,
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(50),
+                      topRight: Radius.circular(50))),
+              width: size.width,
+              height: size.height * 0.75,
+              child: const Text("hello"),
+            ),
+          )
+        ],
       ),
     );
   }
