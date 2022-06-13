38d78b56ccceee19e4ef187e55f7f0e4508ea409
Binh Pham <binh.pham2@mservice.com.vn>
2022-06-13 23:16:08 +0700

    create Hui_Group_Item in page 1

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,3 +1,5 @@
 import 'package:flutter/material.dart';
 
 const PRIMARY_BACKGROUND = Color(0xFFA50064);
+const PINK_TEXT = Color(0xFFA50064);
+const GREEN = Color(0xFF29771C);
--- lib/main.dart
+++ lib/main.dart
@@ -26,19 +26,89 @@ class MyApp extends StatelessWidget {
           Positioned(
             bottom: 0,
             child: Container(
-              alignment: Alignment.center,
-              decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(50),
-                      topRight: Radius.circular(50))),
-              width: size.width,
-              height: size.height * 0.75,
-              child: const Text("hello"),
-            ),
+                padding: const EdgeInsets.symmetric(vertical: 25),
+                alignment: Alignment.topCenter,
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(50),
+                        topRight: Radius.circular(50))),
+                width: size.width,
+                height: size.height * 0.75,
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Hui_Group_Item(size: size),
+                      Hui_Group_Item(size: size),
+                      Hui_Group_Item(size: size),
+                      Hui_Group_Item(size: size),
+                      Hui_Group_Item(size: size),
+                    ],
+                  ),
+                )),
           )
         ],
       ),
     );
+  }
+}
+
+class Hui_Group_Item extends StatelessWidget {
+  const Hui_Group_Item({
+    Key? key,
+    required this.size,
+  }) : super(key: key);
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: const EdgeInsets.only(bottom: 20),
+        alignment: Alignment.topCenter,
+        padding: const EdgeInsets.all(15),
+        decoration: const BoxDecoration(
+            color: Colors.grey,
+            borderRadius: BorderRadius.all(Radius.circular(15))),
+        width: size.width * 0.9,
+        child: Column(children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Hụi phố phường",
+                style: TextStyle(
+                    fontSize: size.height * 0.028, fontWeight: FontWeight.bold),
+              ),
+              IconButton(
+                iconSize: size.height * 0.035,
+                icon: Icon(Icons.favorite),
+                color: Colors.red,
+                onPressed: () {},
+              )
+            ],
+          ),
+          const SizedBox(height: 15),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("40.000d",
+                  style: TextStyle(
+                      fontSize: size.height * 0.025,
+                      fontWeight: FontWeight.bold,
+                      color: PINK_TEXT)),
+              TextButton(
+                style: TextButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 15, horizontal: 25),
+                    primary: Color.fromARGB(217, 255, 255, 255),
+                    backgroundColor: GREEN),
+                child: Text("Active"),
+                onPressed: () {},
+              )
+            ],
+          ),
+        ]));
   }
 }
