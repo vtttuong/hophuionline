c56f106c0a5a44ddb8edaac5284dc934d06fb0ad
duy.ha <duy.ha@mservice.com.vn>
2022-06-16 13:49:18 +0700

    [backend] add connection to database

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,14 @@ from datetime import datetime
 
 import asyncio
 import json
+import psycopg2, os
+
+async def get_db_conn():
+    return psycopg2.connect(
+        database=os.getenv("DB_DATABASE"),
+        user=os.getenv("DB_USERNAME"), password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST"), port= os.getenv("DB_PORT"), sslmode=os.getenv("DB_SSL_MODE")
+    )
 
 app = FastAPI()
 
@@ -17,6 +25,19 @@ def read_root():
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
+@app.get("/db_version")
+async def db_version():
+
+    conn = await get_db_conn()
+    cursor = conn.cursor()
+    cursor.execute("select version()")
+
+    data = cursor.fetchone()
+
+    # please close conn manually
+    conn.close()
+
+    return f"Connection established to: {data}"
 
 @app.post('/results')
 async def get_body(request: Request):
